fc70dbe668b42383eff41ef34108da887063f7e3
Patrick Titzler <ptitzler@us.ibm.com>
2017-11-09 12:58:01 -0800

    initial

--- src/python-flask/server/routes/usecase_route.py
+++
@@ -1,9 +0,0 @@
-from server import app
-from server.services import service_manager
-
-# service_sdk = service_manager.get('service-name')
-
-@app.route('/usecase')
-def usecase():
-    """usecase route"""
-    return 'hello from usecase_route'
